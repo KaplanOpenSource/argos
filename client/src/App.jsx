061b99bee759ecb256c43a6076e9902ff7af4b37
Eran Geva <eran.geva@gmail.com>
2024-01-23 16:20:22 +0100

    MapShower

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
+import { MapShower } from './MapShower'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1>Vite + React</h1>
+    <div
+      // style={{top:0, bottom:0, position:'100vfh'}}
+      // style={{ height: "100%", width: '100%', position: 'absolute', top: 0, bottom: 0, right: 0 }}
+    >
+      {/* <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-      </div>
+      </div> */}
+      <MapShower>
+
+      </MapShower>
     </div>
   )
 }
---
+++ client/src/MapShower.jsx
@@ -0,0 +1,28 @@
+import { CRS } from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+import { MapContainer, TileLayer, ZoomControl } from 'react-leaflet'
+
+export const MapShower = ({ children }) => {
+    return (
+        <MapContainer
+            zoom={15}
+            // style={{ height: "1000%" }}
+            style={{ height: "100%", width: '100%', position: 'absolute', top: 0, bottom: 0, right: 0 }}
+            crs={CRS.EPSG3857}
+            // bounds={posbounds}
+            center={[51.505, -0.09]}
+            zoomControl={false}
+            minZoom={-6}
+            maxZoom={30}
+            contextmenu={true}
+        >
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <ZoomControl position='bottomright' />
+            {children}
+        </MapContainer>
+    );
+}
