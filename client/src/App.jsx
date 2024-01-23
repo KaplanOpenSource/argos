d5ca43ec385d74405a9ad3c0bda24a0274ee21e4
Eran Geva <eran@kaplanopensource.co.il>
2024-01-23 18:54:41 +0100

    list experiments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,11 @@ import { useState } from 'react'
 import { MapShower } from './MapShower'
 
 import * as React from 'react';
-import Button from '@mui/material/Button';
+import {
+  AppBar, IconButton, Toolbar, Typography, Button
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useEffect } from 'react';
 
 function ButtonUsage() {
   return <Button variant="contained">Hello world</Button>;
@@ -10,11 +14,22 @@ function ButtonUsage() {
 
 function App() {
   const [count, setCount] = useState(0)
+  const [experiments, setExperiments] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const resp = await fetch("http://127.0.0.1:8080/experiment_names");
+      const json = await resp.json();
+      setExperiments((json || []).map(x => {
+        return { name: x, data: {} };
+      }))
+    })()
+  }, [])
 
   return (
     <div
-      // style={{top:0, bottom:0, position:'100vfh'}}
-      // style={{ height: "100%", width: '100%', position: 'absolute', top: 0, bottom: 0, right: 0 }}
+    // style={{top:0, bottom:0, position:'100vfh'}}
+    // style={{ height: "100%", width: '100%', position: 'absolute', top: 0, bottom: 0, right: 0 }}
     >
       {/* <h1>Vite + React</h1>
       <div className="card">
@@ -25,7 +40,31 @@ function App() {
       {/* <MapShower>
 
       </MapShower> */}
-      <ButtonUsage></ButtonUsage>
+      {/* <ButtonUsage></ButtonUsage> */}
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Argos
+          </Typography>
+          {/* <Button color="inherit">Login</Button> */}
+        </Toolbar>
+      </AppBar>
+      {
+        experiments.map(e => (
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            {e.name}
+          </Typography>
+        ))
+      }
     </div>
   )
 }
--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 import argparse
 import os
 import re
-from flask import Flask, send_from_directory
+import json
+from flask import Flask, send_from_directory, request
 from flask_cors import CORS, cross_origin
 
+EXPERIMENTS_PATH = "data/experiments"
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--port", default=8080, help="needs to be synced with client port, look at client/src/constants.js")
@@ -14,7 +16,7 @@ print(args)
 app = Flask(__name__, static_url_path="/", static_folder="client/dist")
 # template_folder='web/templates')
 
-cors_origins = '*'
+cors_origins = "*"
 if args.prod:
     cors_origins = []
 cors = CORS(app, origins=cors_origins)
@@ -28,6 +30,33 @@ def index_func():
 @app.route("/<path:path>")
 def static_file(path):
     return app.send_static_file(path)
+
+
+@app.route("/experiment_names")
+def experimentNamesReq():
+    if not os.path.exists(EXPERIMENTS_PATH):
+        return []
+    names = sorted(os.listdir(EXPERIMENTS_PATH))
+    names = [os.path.splitext(n)[0] for n in names]
+    return names
+
+
+@app.route("/experiment/<name>")
+def experimentGetReq(name):
+    if re.match("^[0-9_a-zA-Z]+$", name):
+        if os.path.exists(os.path.join(EXPERIMENTS_PATH, name + ".json")):
+            return send_from_directory(EXPERIMENTS_PATH, name + ".json")
+    return {"error": "unknown experiment name"}
+
+
+@app.route("/set_experiment/<name>", methods=["POST"])
+def experimentSetReq(name):
+    if re.match("^[0-9_a-zA-Z]+$", name):
+        os.makedirs(EXPERIMENTS_PATH, exist_ok=True)
+        json_data = json.loads(request.form["json"])
+        with open(os.path.join(EXPERIMENTS_PATH, name + ".json"), "w") as file:
+            file.write(json_data)
+    return {"error": "invalid experiment name"}
 
 
 if __name__ == "__main__":  # pragma: no cover
