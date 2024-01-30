4116ccbf34b508719dfb09b5ac315575ca4977e1
Eran Geva <eran@kaplanopensource.co.il>
2024-01-30 19:52:31 +0100

    entity type

--- client/src/ExperimentRow.jsx
+++ client/src/ExperimentRow.jsx
@@ -5,16 +5,16 @@ import dayjs from 'dayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { TrailSet } from "./TrailSet";
 import { TreeRow } from "./TreeRow";
+import { EntityType } from "./EntityType";
 
 export const ExperimentRow = ({ name, data, setData }) => {
-    const newTrialSetName = () => {
-        const newName = 'new_trial_set';
-        if (!(data.trailSet || []).find(t => t.name === newName)) {
-            return newName
+    const newName = (items, nameTemplate) => {
+        if (!(items || []).find(t => t.name === nameTemplate)) {
+            return nameTemplate
         }
         for (let i = 1; ; ++i) {
-            if (!(data.trailSet || []).find(t => t.name === newName + '_' + i)) {
-                return newName + '_' + i;
+            if (!(items || []).find(t => t.name === nameTemplate + '_' + i)) {
+                return nameTemplate + '_' + i;
             }
         }
     }
@@ -25,6 +25,14 @@ export const ExperimentRow = ({ name, data, setData }) => {
         i = i >= 0 ? i : trailSet.length;
         trailSet[i] = { name: trialName, data: trialNewData };
         setData({ ...data, trailSet });
+    }
+
+    const setEntityTypeData = (theName, theData) => {
+        const entityTypes = (data.entityTypes || []).slice();
+        let i = entityTypes.findIndex(t => t.name === theName);
+        i = i >= 0 ? i : entityTypes.length;
+        entityTypes[i] = { name: theName, data: theData };
+        setData({ ...data, entityTypes });
     }
 
     return (
@@ -47,16 +55,15 @@ export const ExperimentRow = ({ name, data, setData }) => {
                         value={dayjs(data.endDate)}
                         onChange={(val) => setData({ ...data, endDate: val })}
                     />
-                    <IconButton
-                        // edge="start"
-                        color="inherit"
-                        onClick={() => setTrialSetData(newTrialSetName(), {})}
-                    >
-                        <AddIcon />
-                    </IconButton>
                 </>
             }
         >
+            <IconButton
+                color="inherit"
+                onClick={() => setTrialSetData(newName(data.trailSet, 'new_trial_set'), {})}
+            >
+                <AddIcon />
+            </IconButton>
             {
                 (data.trailSet || []).map(e => (
                     <TrailSet
@@ -64,6 +71,22 @@ export const ExperimentRow = ({ name, data, setData }) => {
                         name={e.name}
                         data={e.data}
                         setData={newData => setTrialSetData(e.name, newData)}
+                    />
+                ))
+            }
+            <IconButton
+                color="inherit"
+                onClick={() => setEntityTypeData(newName(data.entityTypes, 'new_entity_type'), {})}
+            >
+                <AddIcon />
+            </IconButton>
+            {
+                (data.entityTypes || []).map(e => (
+                    <EntityType
+                        key={e.name}
+                        name={e.name}
+                        data={e.data}
+                        setData={newData => setEntityTypeData(e.name, newData)}
                     />
                 ))
             }
