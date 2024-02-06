9f1dc6fe2287d8d3b30b0e6c7f57ec3708e7a64a
Eran Geva <eran@kaplanopensource.co.il>
2024-02-06 08:39:47 +0100

    TreeSublist is TreeItem

--- client/src/TreeSublist.jsx
+++ client/src/TreeSublist.jsx
@@ -2,46 +2,51 @@ import { Box, IconButton, Tooltip, Typography } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 
 import { camelCaseToWords, createNewName } from "./utils";
+import { TreeItem } from "@mui/x-tree-view/TreeItem";
 
 export const TreeSublist = ({ nameTemplate, fieldName, data, setData, component, newDataCreator }) => {
     const items = data[fieldName] || [];
 
     return (
-        <>
-            <Box
-                sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    alignContent: 'flex-start',
-                    // p: 0.5,
-                    // pr: 0,
-                }}
-            >
-                <Typography variant="body2" sx={{
-                    fontWeight: 'inherit',
-                    //  flexGrow: 1
-                }}>
-                    {camelCaseToWords(fieldName)}
-                </Typography>
-                <Tooltip title="Add New" placement="right">
-                    <IconButton
-                        sx={{
-                            display: 'flex',
-                            alignSelf: 'flex-start',
-                            alignContent: 'flex-start',
-                            alignItems: 'flex-start',
-                        }}
-                        color="inherit"
-                        onClick={() => {
-                            const theData = newDataCreator ? newDataCreator() : {};
-                            theData.name = createNewName(items, nameTemplate);
-                            setData({ ...data, [fieldName]: [...items, theData] });
-                        }}
-                    >
-                        <AddIcon />
-                    </IconButton>
-                </Tooltip>
-            </Box>
+        <TreeItem
+            nodeId={fieldName}
+            label={
+                <Box
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        alignContent: 'flex-start',
+                        // p: 0.5,
+                        // pr: 0,
+                    }}
+                >
+                    <Typography variant="body2" sx={{
+                        fontWeight: 'inherit',
+                        //  flexGrow: 1
+                    }}>
+                        {camelCaseToWords(fieldName)}
+                    </Typography>
+                    <Tooltip title="Add New" placement="right">
+                        <IconButton
+                            sx={{
+                                display: 'flex',
+                                alignSelf: 'flex-start',
+                                alignContent: 'flex-start',
+                                alignItems: 'flex-start',
+                            }}
+                            color="inherit"
+                            onClick={() => {
+                                const theData = newDataCreator ? newDataCreator() : {};
+                                theData.name = createNewName(items, nameTemplate);
+                                setData({ ...data, [fieldName]: [...items, theData] });
+                            }}
+                        >
+                            <AddIcon />
+                        </IconButton>
+                    </Tooltip>
+                </Box>
+            }
+        >
             {
                 items.map(itemData => (
                     component(
@@ -55,6 +60,6 @@ export const TreeSublist = ({ nameTemplate, fieldName, data, setData, component,
                     )
                 ))
             }
-        </>
+        </TreeItem>
     )
 }
