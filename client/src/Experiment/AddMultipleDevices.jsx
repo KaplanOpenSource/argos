31fb3225ff229077781430e5d8f96e41e99a0d06
Eran Geva <eran@kaplanopensource.co.il>
2024-03-07 13:01:02 +0100

    multiple add with Popover and attributes

--- client/src/Experiment/AddMultipleDevices.jsx
+++ client/src/Experiment/AddMultipleDevices.jsx
@@ -1,20 +1,25 @@
 import { DynamicFeed, PlayArrow } from "@mui/icons-material"
-import { IconButton, Tooltip } from "@mui/material"
+import { Box, IconButton, Paper, Popover, Stack, Tooltip } from "@mui/material"
 import { useState } from "react"
 import { TextFieldDebounce } from "../Utils/TextFieldDebounce";
+import { AttributeItemList } from "./AttributeItemList";
+import { TreeItem } from "@mui/x-tree-view/TreeItem";
 
 export const AddMultipleDevices = ({ deviceType, addDevices }) => {
-    const [open, setOpen] = useState(false);
+    const [anchorEl, setAnchorEl] = useState(null);
     const [instances, setInstances] = useState(10);
     const [prefix, setPrefix] = useState(deviceType.name + "_");
     const [digits, setDigits] = useState(3);
     const [suffix, setSuffix] = useState("");
+    const [attrValues, setAttrValues] = useState([]);
+
+    const open = Boolean(anchorEl);
 
     const createNewDevices = () => {
         const newDevices = [];
         for (let i = 1; i <= instances; ++i) {
             const name = prefix + ('' + i).padStart(digits, '0') + suffix;
-            newDevices.push({ name });
+            newDevices.push({ name, ...attrValues });
         }
         addDevices(newDevices);
         setOpen(false);
@@ -27,72 +32,95 @@ export const AddMultipleDevices = ({ deviceType, addDevices }) => {
                     color={open ? "primary" : ""}
                     onClick={(e) => {
                         e.stopPropagation();
-                        setOpen(!open)
+                        setAnchorEl(e.currentTarget);
                     }}
                 >
                     <DynamicFeed />
                 </IconButton>
             </Tooltip>
-            {open && (
-                <>
-                    <TextFieldDebounce
-                        sx={{
-                            width: 120,
-                        }}
-                        variant='outlined'
-                        label="New devices"
-                        type="number"
-                        size='small'
-                        InputLabelProps={{ shrink: true }}
-                        onChange={val => setInstances(val)}
-                        value={instances}
-                    />
-                    <TextFieldDebounce
-                        sx={{
-                            width: 150,
-                        }}
-                        variant='outlined'
-                        label="Name prefix"
-                        size='small'
-                        InputLabelProps={{ shrink: true }}
-                        onChange={val => setPrefix(val)}
-                        value={prefix}
-                    />
-                    <TextFieldDebounce
-                        sx={{
-                            width: 100,
-                        }}
-                        variant='outlined'
-                        label="Digits"
-                        type="number"
-                        size='small'
-                        InputLabelProps={{ shrink: true }}
-                        onChange={val => setDigits(val)}
-                        value={digits}
-                    />
-                    <TextFieldDebounce
-                        sx={{
-                            width: 150,
-                        }}
-                        variant='outlined'
-                        label="Name suffix"
-                        size='small'
-                        InputLabelProps={{ shrink: true }}
-                        onChange={val => setSuffix(val)}
-                        value={suffix}
-                    />
-                    <Tooltip title={"Create"} placement="right">
-                        <IconButton
-                            onClick={(e) => {
-                                e.stopPropagation();
-                                createNewDevices();
+            <Popover
+                // id={id}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={() => setAnchorEl(null)}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+            >
+                <Stack
+                    sx={{ padding: '5px', borderColor: 'black', border: 2 }}
+                >
+                    <Box>
+
+                        <TextFieldDebounce
+                            sx={{
+                                width: 120,
                             }}
-                        >
-                            <PlayArrow />
-                        </IconButton>
-                    </Tooltip>
-                </>
-            )}
+                            variant='outlined'
+                            label="New devices"
+                            type="number"
+                            size='small'
+                            InputLabelProps={{ shrink: true }}
+                            onChange={val => setInstances(val)}
+                            value={instances}
+                        />
+                        <TextFieldDebounce
+                            sx={{
+                                width: 150,
+                            }}
+                            variant='outlined'
+                            label="Name prefix"
+                            size='small'
+                            InputLabelProps={{ shrink: true }}
+                            onChange={val => setPrefix(val)}
+                            value={prefix}
+                        />
+                        <TextFieldDebounce
+                            sx={{
+                                width: 100,
+                            }}
+                            variant='outlined'
+                            label="Digits"
+                            type="number"
+                            size='small'
+                            InputLabelProps={{ shrink: true }}
+                            onChange={val => setDigits(val)}
+                            value={digits}
+                        />
+                        <TextFieldDebounce
+                            sx={{
+                                width: 150,
+                            }}
+                            variant='outlined'
+                            label="Name suffix"
+                            size='small'
+                            InputLabelProps={{ shrink: true }}
+                            onChange={val => setSuffix(val)}
+                            value={suffix}
+                        />
+                    </Box>
+                    <Box>
+                        <AttributeItemList
+                            attributeTypes={deviceType.attributeTypes}
+                            data={attrValues}
+                            setData={setAttrValues}
+                        />
+                    </Box>
+                    <Box>
+                        <Tooltip title={"Create"} placement="right">
+                            <IconButton
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    createNewDevices();
+                                }}
+                            >
+                                <PlayArrow />
+                            </IconButton>
+                        </Tooltip>
+                    </Box>
+                </Stack>
+            </Popover>
         </>
     )
 }
