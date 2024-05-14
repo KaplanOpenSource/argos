f12e82d2b434df4051e61acf29dd59ce8ba2e106
Eran Geva <eran@kaplanopensource.co.il>
2024-05-14 13:18:23 +0200

    theme on toggle group

--- client/src/App/ShowOnlyButtonGroup.jsx
+++ client/src/App/ShowOnlyButtonGroup.jsx
@@ -1,7 +1,13 @@
 import { CellTower, Terrain } from "@mui/icons-material";
 import { useContext } from "react";
 import { experimentContext } from "../Context/ExperimentProvider";
-import { ToggleButton, ToggleButtonGroup, Tooltip } from "@mui/material";
+import { ThemeProvider, ToggleButton, ToggleButtonGroup, Tooltip, createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: { main: '#fefefe' }
+    },
+});
 
 export const ShowOnlyButtonGroup = ({ showDevicesOnly, setShowDevicesOnly }) => {
     const {
@@ -9,33 +15,35 @@ export const ShowOnlyButtonGroup = ({ showDevicesOnly, setShowDevicesOnly }) => 
     } = useContext(experimentContext);
 
     return (
-        <Tooltip
-            title={currTrial.experiment
-                ? ""
-                : "Choose an experiment to customize which parts of the experiment are shown"}
-        >
-            <ToggleButtonGroup
-                color="warning"
-                size="small"
-                value={showDevicesOnly}
-                exclusive
-                onChange={(e, val) => {
-                    e.stopPropagation();
-                    setShowDevicesOnly(val);
-                }}
-                disabled={!currTrial.experiment}
+        <ThemeProvider theme={theme}>
+            <Tooltip
+                title={currTrial.experiment
+                    ? ""
+                    : "Choose an experiment to customize which parts of the experiment are shown"}
             >
-                <ToggleButton value={false}>
-                    <Tooltip title="Show all experiments and trials">
-                        <Terrain />
-                    </Tooltip>
-                </ToggleButton>
-                <ToggleButton value={true}>
-                    <Tooltip title="Show only devices of current experiment">
-                        <CellTower />
-                    </Tooltip>
-                </ToggleButton>
-            </ToggleButtonGroup>
-        </Tooltip>
+                <ToggleButtonGroup
+                    color="primary"
+                    size="small"
+                    value={showDevicesOnly}
+                    exclusive
+                    onChange={(e, val) => {
+                        e.stopPropagation();
+                        setShowDevicesOnly(val);
+                    }}
+                    disabled={!currTrial.experiment}
+                >
+                    <ToggleButton value={false}>
+                        <Tooltip title="Show all experiments and trials">
+                            <Terrain />
+                        </Tooltip>
+                    </ToggleButton>
+                    <ToggleButton value={true}>
+                        <Tooltip title="Show only devices of current experiment">
+                            <CellTower />
+                        </Tooltip>
+                    </ToggleButton>
+                </ToggleButtonGroup>
+            </Tooltip>
+        </ThemeProvider>
     )
 }
