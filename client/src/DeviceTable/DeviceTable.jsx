ccb499e922ccd671d3c886763c00ee75c2ce4dbc
Eran Geva <eran@kaplanopensource.co.il>
2024-05-21 09:27:17 +0200

    when not showing attribute, show minimal selected device card

--- client/src/DeviceTable/DeviceTable.jsx
+++ client/src/DeviceTable/DeviceTable.jsx
@@ -1,13 +1,13 @@
-import { Box, Button, Paper } from "@mui/material"
+import { Paper, Stack, Typography } from "@mui/material"
 import { TreeView } from "@mui/x-tree-view/TreeView"
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import AccountTreeIcon from '@mui/icons-material/AccountTree';
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { experimentContext } from "../Context/ExperimentProvider";
 import { DeviceItem } from "../Experiment/DeviceItem";
-import { ButtonTooltip } from "../Utils/ButtonTooltip";
 import { SCOPE_TRIAL } from "../Experiment/AttributeType";
+import { SelectDeviceButton } from "../Experiment/SelectDeviceButton";
+import { DeviceItemLocationButton } from "../Experiment/DeviceItemLocationButton";
 
 export const DeviceTable = ({ showAttributes, setShowAttributes }) => {
     const { selection, currTrial } = useContext(experimentContext);
@@ -17,40 +17,82 @@ export const DeviceTable = ({ showAttributes, setShowAttributes }) => {
         const deviceType = (currTrial.experiment || {}).deviceTypes.find(x => x.name === deviceTypeName);
         const deviceItem = ((deviceType || {}).devices || []).find(x => x.name === deviceItemName);
         if (deviceType && deviceItem) {
-            shownDevices.push({ deviceType, deviceItem, deviceTypeName, deviceItemName });
+            shownDevices.push({ deviceType, deviceItem });
         }
     };
 
     return (
-        <TreeView
-            defaultCollapseIcon={<ExpandMoreIcon />}
-            defaultExpandIcon={<ChevronRightIcon />}
-            sx={{
-                height: '80vh',
-                overflowY: 'auto',
-            }}
-            disableSelection
-        >
-            {shownDevices.map(({ deviceType, deviceItem, deviceTypeName, deviceItemName }) => (
-                <Paper sx={{
-                    zIndex: 1000,
-                    position: 'relative',
-                    maxWidth: 'fit-content',
-                    maxHeight: 'fit-content',
-                    right: '10px',
-                    top: '5px',
-                }}>
-                    <DeviceItem
-                        key={deviceItemName}
-                        data={deviceItem}
-                        // setData={setData}
-                        deviceType={deviceType}
-                        showAttributes={showAttributes}
-                        devicesEnclosingList={shownDevices}
-                        scope={SCOPE_TRIAL}
-                    />
-                </Paper>
-            ))}
-        </TreeView>
+        <>
+            {showAttributes
+                ? <TreeView
+                    defaultCollapseIcon={<ExpandMoreIcon />}
+                    defaultExpandIcon={<ChevronRightIcon />}
+                    sx={{
+                        height: '80vh',
+                        overflowY: 'auto',
+                    }}
+                    disableSelection
+                >
+                    {shownDevices.map(({ deviceType, deviceItem }) => (
+                        <Paper
+                            key={deviceType.name + ":" + deviceItem.name}
+                            sx={{
+                                zIndex: 1000,
+                                position: 'relative',
+                                maxWidth: 'fit-content',
+                                maxHeight: 'fit-content',
+                                right: '10px',
+                                top: '5px',
+                            }}
+                        >
+                            <DeviceItem
+                                data={deviceItem}
+                                deviceType={deviceType}
+                                devicesEnclosingList={shownDevices}
+                                scope={SCOPE_TRIAL}
+                                showAttributes={true}
+                            />
+                        </Paper>
+                    ))}
+                </TreeView>
+                : <Stack
+                    direction='column'
+                    alignItems="flex-end"
+                    sx={{
+                        zIndex: 1000,
+                        height: '80vh',
+                        overflowY: 'auto',
+                    }}
+                >
+                    {shownDevices.map(({ deviceType, deviceItem }) => (
+                        <Paper
+                            sx={{
+                                paddingLeft: '5px',
+                                maxWidth: 'fit-content'
+                            }}
+                        >
+                            <Stack
+                                direction='row'
+                                alignItems='center'
+                                justifyContent="end"
+                            >
+                                <Typography>
+                                    {deviceItem.name}
+                                </Typography>
+                                <SelectDeviceButton
+                                    deviceItem={deviceItem}
+                                    deviceType={deviceType}
+                                    devicesEnclosingList={shownDevices}
+                                />
+                                <DeviceItemLocationButton
+                                    deviceType={deviceType}
+                                    deviceItem={deviceItem}
+                                />
+                            </Stack>
+                        </Paper>
+                    ))}
+                </Stack>
+            }
+        </>
     )
 }
