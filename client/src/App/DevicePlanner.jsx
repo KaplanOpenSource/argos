b1af6362029cb108b00c105bd5c0aed48abde4ce
Eran Geva <eran@kaplanopensource.co.il>
2024-05-27 17:22:56 +0200

    fix crush on no EnclosingListSelection for device markers with another context instance

--- client/src/App/DevicePlanner.jsx
+++ client/src/App/DevicePlanner.jsx
@@ -58,8 +58,10 @@ export function DevicePlanner() {
                     markedPoints={markedPoints}
                     setMarkedPoints={setMarkedPoints}
                 />
-                <DeviceMarkersShown
-                />
+                <EnclosingListSelectionProvider>
+                    <DeviceMarkersShown
+                    />
+                </EnclosingListSelectionProvider>
                 <ActionsOnMapDoer
                 />
 
--- client/src/Experiment/EnclosedSelectionProvider.jsx
+++ client/src/Experiment/EnclosedSelectionProvider.jsx
@@ -2,13 +2,13 @@ import { createContext, useState } from "react"
 
 export const EnclosingListSelectionContext = createContext();
 
-export const EnclosingListSelectionProvider = ({ children }) => {
-    const [selectionOnEnclosingUuids, setSelectionOnEnclosingUuids] = useState([]);
+export const EnclosingListSelectionProvider = ({ noSelection = false, children }) => {
+    const [selectionOnEnclosingUuids, setSelectionOnEnclosingUuids] = useState(noSelection ? undefined : []);
     return (
         <EnclosingListSelectionContext.Provider
             value={{
                 selectionOnEnclosingUuids,
-                setSelectionOnEnclosingUuids,
+                setSelectionOnEnclosingUuids: noSelection ? () => { } : setSelectionOnEnclosingUuids,
             }}
         >
             {children}
