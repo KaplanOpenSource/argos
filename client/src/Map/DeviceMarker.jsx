2716332d03122d540111e72edf88ee1ca0360a3d
Eran Geva <eran@kaplanopensource.co.il>
2024-06-03 09:40:51 +0200

    marker icon with name

--- client/src/Map/DeviceMarker.jsx
+++ client/src/Map/DeviceMarker.jsx
@@ -3,6 +3,10 @@ import { SingleDevicePropertiesView } from "./SingleDevicePropertiesView";
 import { useContext, useEffect, useRef } from "react";
 import { usePopupSwitch } from "./PopupSwitchContext";
 import { experimentContext } from "../Context/ExperimentProvider";
+import { renderToStaticMarkup } from "react-dom/server";
+import { divIcon } from "leaflet";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 
 export const DeviceMarker = ({ deviceOnTrial, setDeviceOnTrial }) => {
     const { setLocationsToDevices } = useContext(experimentContext);
@@ -26,12 +30,37 @@ export const DeviceMarker = ({ deviceOnTrial, setDeviceOnTrial }) => {
         return coords.map(x => Math.round(x * 1e8) / 1e8).join(',')
     }
 
+    const icon = divIcon({
+        className: 'argos-leaflet-div-icon',
+        iconSize: [20, 20],
+        iconAnchor: [10, 22],
+        html: renderToStaticMarkup(
+            <div>
+                <FontAwesomeIcon
+                    icon={faMapMarkerAlt}
+                    size="xl"
+                    color="#297A31"
+                />
+                {/* style={{ color: '#297A31' }} */}
+                {/* <i className=" fa fa-map-marker-alt fa-2x"
+            // style={{ color: (isTypeSelected ? (isSelected ? '#297A31' : '#1B2C6F') : '#888888') }}
+            /> */}
+                {/* {!shouldShowName ? null : */}
+                <span style={{ backgroundColor: "#fafa44", marginTop: 5, padding: 3, borderColor: "black", color: '#ff4466' }}>
+                    {deviceItemName.replace(/ /g, '\u00a0')}
+                </span>
+                {/* } */}
+            </div>
+        )
+    });
+
     return (
         <Marker
             key={deviceTypeName + '_' + deviceItemName}
             position={coordinates}
             ref={ref}
             draggable={true}
+            icon={icon}
             eventHandlers={{
                 dragend: e => {
                     // e.target.closeTooltip();
