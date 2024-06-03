a8b2f30795127c41948c043c6e19e1f9eeec4672
Eran Geva <eran@kaplanopensource.co.il>
2024-06-03 10:48:49 +0200

    select by AreaMarkListener

--- client/src/Map/DeviceMarkersShown.jsx
+++ client/src/Map/DeviceMarkersShown.jsx
@@ -3,9 +3,10 @@ import { experimentContext } from "../Context/ExperimentProvider";
 import { RealMapName } from "../constants/constants";
 import { DeviceMarker } from "./DeviceMarker";
 import { PopupSwitchProvider } from "./PopupSwitchContext";
+import { AreaMarkListener } from "./AreaMarkListener";
 
 export const DeviceMarkersShown = ({ showDeviceNames }) => {
-    const { currTrial, setTrialData } = useContext(experimentContext);
+    const { currTrial, setTrialData, selection, setSelection } = useContext(experimentContext);
     const devicesOnTrial = (currTrial.trial || {}).devicesOnTrial || [];
     const mapName = currTrial.shownMapName || RealMapName;
     const devicesWithoutLocation = devicesOnTrial.filter(({ location }) => !location || !location.coordinates);
@@ -14,19 +15,37 @@ export const DeviceMarkersShown = ({ showDeviceNames }) => {
     }
     const shownDevices = devicesOnTrial.filter(({ location }) => location && location.coordinates && location.name === mapName);
     return (
-        <PopupSwitchProvider>
-            {shownDevices.map((deviceOnTrial, index) => (
-                <DeviceMarker
-                    key={index}
-                    deviceOnTrial={deviceOnTrial}
-                    setDeviceOnTrial={newDeviceData => {
-                        const data = { ...currTrial.trial, devicesOnTrial: devicesOnTrial.slice() };
-                        data.devicesOnTrial[index] = newDeviceData;
-                        setTrialData(data);
-                    }}
-                    showDeviceNames={showDeviceNames}
-                />
-            ))}
-        </PopupSwitchProvider>
+        <>
+            <PopupSwitchProvider>
+                {shownDevices.map((deviceOnTrial, index) => (
+                    <DeviceMarker
+                        key={index}
+                        deviceOnTrial={deviceOnTrial}
+                        setDeviceOnTrial={newDeviceData => {
+                            const data = { ...currTrial.trial, devicesOnTrial: devicesOnTrial.slice() };
+                            data.devicesOnTrial[index] = newDeviceData;
+                            setTrialData(data);
+                        }}
+                        showDeviceNames={showDeviceNames}
+                    />
+                ))}
+            </PopupSwitchProvider>
+            <AreaMarkListener
+                onAreaMarked={({ boxZoomBounds }) => {
+                    const added = [];
+                    for (const { deviceItemName, deviceTypeName, location } of shownDevices) {
+                        if (boxZoomBounds.contains(location.coordinates)) {
+                            const isSelected = selection.find(s => {
+                                return s.deviceItemName === deviceItemName && s.deviceTypeName === deviceTypeName
+                            });
+                            if (!isSelected) {
+                                added.push({deviceItemName, deviceTypeName});
+                            }
+                        }
+                    }
+                    setSelection([...selection, ...added]);
+                }}
+            />
+        </>
     )
 }
