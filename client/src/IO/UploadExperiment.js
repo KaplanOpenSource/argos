a40f1408994d001750a3fb1103be8abab629fe71
Eran Geva <eran@kaplanopensource.co.il>
2024-06-25 14:22:42 +0200

    useUploadExperiment as a custom hook

--- client/src/IO/UploadExperiment.js
+++ client/src/IO/UploadExperiment.js
@@ -3,26 +3,25 @@ import { UploadImage } from "./UploadImage";
 import { ConvertExperiment, isExperimentVersion2 } from "./ConvertExperiment";
 import { argosJsonVersion } from "../constants/constants";
 import { createNewName } from "../Utils/utils";
+import { useCallback, useContext } from "react";
+import { experimentContext } from "../Context/ExperimentProvider";
 
-export class UploadExperiment {
-    constructor(addExperiment, setExperiment) {
-        this.addExperiment = addExperiment;
-        this.setExperiment = setExperiment;
-    }
+export const useUploadExperiment = () => {
+    const { experiments, addExperiment } = useContext(experimentContext);
 
-    async go(file, experiments) {
+    const uploadExperiment = useCallback(async (file) => {
         if (!file) {
             throw "empty file";
         }
 
-        const rawExp = await this.readExperiment(file);
-        const experiment = this.checkConvert(rawExp);
+        const { rawExp, zip } = await readExperiment(file);
+        const experiment = checkConvert(rawExp);
 
         const name = createNewName(experiments, experiment.name); // this is done here to upload images with the correct experiment name
         experiment.name = name;
 
-        if (this.zip) {
-            const imageFiles = Object.values(this.zip.files).filter(x => x.name.startsWith('images/') && !x.dir);
+        if (zip) {
+            const imageFiles = Object.values(zip.files).filter(x => x.name.startsWith('images/') && !x.dir);
             if (imageFiles.length > 0) {
                 const images = (experiment.imageStandalone || []).concat((experiment.imageEmbedded || []));
                 for (const im of imageFiles) {
@@ -44,10 +43,10 @@ export class UploadExperiment {
             }
         }
 
-        this.addExperiment(experiment);
-    }
+        addExperiment(experiment);
+    }, [experiments, addExperiment]);
 
-    async readExperiment(file) {
+    const readExperiment = async (file) => {
         if (file.name.endsWith('.json')) {
             const text = await new Promise(resolve => {
                 const reader = new FileReader();
@@ -55,20 +54,19 @@ export class UploadExperiment {
                 reader.readAsText(file);
             });
             const experiment = JSON.parse(text);
-            return experiment || {};
+            return { rawExp: experiment || {}, zip: undefined };
         }
         if (file.name.endsWith('.zip')) {
-            this.zip = await JSZip().loadAsync(file);
-            const jsonFile = Object.values(this.zip.files).filter(x => x.name.endsWith('.json'))[0];
-            // console.log(this.zip);
+            const zip = await JSZip().loadAsync(file);
+            const jsonFile = Object.values(zip.files).filter(x => x.name.endsWith('.json'))[0];
             const text = await jsonFile.async('text');
             const experiment = JSON.parse(text);
-            return experiment || {};
+            return { rawExp: experiment || {}, zip };
         }
         throw "unknown file extension " + file.name;
     }
 
-    checkConvert(experiment) {
+    const checkConvert = (experiment) => {
         const version = (experiment || {}).version;
         if (version === argosJsonVersion) {
             return experiment;
@@ -79,5 +77,9 @@ export class UploadExperiment {
             console.log('error', experiment);
             throw "unknown experiment version";
         }
+    }
+
+    return {
+        uploadExperiment,
     }
 }
--- client/src/IO/UploadExperimentIcon.jsx
+++ client/src/IO/UploadExperimentIcon.jsx
@@ -1,20 +1,19 @@
-import React, { useContext, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import HourglassBottomIcon from '@mui/icons-material/HourglassBottom';
 import { Upload } from "@mui/icons-material";
 import { ButtonTooltip } from "../Utils/ButtonTooltip";
-import { experimentContext } from "../Context/ExperimentProvider";
-import { UploadExperiment } from "./UploadExperiment";
+import { useUploadExperiment } from "./UploadExperiment";
 
 export const UploadExperimentIcon = ({ }) => {
     const inputFile = useRef(null);
-    const { experiments, addExperiment, setExperiment } = useContext(experimentContext);
+    const { uploadExperiment } = useUploadExperiment();
     const [working, setWorking] = useState(false);
 
     const handleChangeFile = async (event) => {
         try {
             setWorking(true);
             const file = event.target.files[0];
-            new UploadExperiment(addExperiment, setExperiment).go(file, experiments);
+            uploadExperiment(file);
         } catch (error) {
             console.log(error)
             alert(`problem uploading:\n${error}`)
