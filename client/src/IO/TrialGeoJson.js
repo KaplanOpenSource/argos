b6e537cfac5f001197199373b2cc4a2bb6d245d5
Eran Geva <eran@kaplanopensource.co.il>
2024-07-09 11:02:46 +0200

    flatten download trial grojson

--- client/src/IO/TrialGeoJson.js
+++ client/src/IO/TrialGeoJson.js
@@ -1,33 +1,47 @@
 import { useCallback } from "react";
+import { AttributeValueGet } from "../Experiment/AttributeValueGet";
+import { SCOPE_TRIAL } from "../Experiment/AttributeType";
 
 export const useTrialGeoJson = () => {
     const downloadGeojson = useCallback((data, experiment, trialType) => {
-        const devicesOnTrial = [...(data.devicesOnTrial || [])].filter(d => {
-            return d.location && d.location.coordinates && d.location.coordinates.length === 2;
-        });
-
         const json = {
             type: 'FeatureCollection',
-            features: devicesOnTrial.map(d => {
+            features: [],
+        };
+
+        for (const d of (data.devicesOnTrial || [])) {
+            if (d.location && d.location.coordinates && d.location.coordinates.length === 2) {
                 const coordinates = d.location.coordinates.slice().reverse();
                 const properties = {
                     name: d.deviceItemName,
                     type: d.deviceTypeName,
                     MapName: d.location.name || RealMapName,
                 };
-                for (const { name, value } of d.attributes || []) {
-                    properties[name] = value;
+
+                const deviceType = experiment?.deviceTypes?.find(x => x.name === d.deviceTypeName);
+                const deviceItem = deviceType?.devices?.find(x => x.name === d.deviceItemName);
+
+                if (deviceItem) {
+                    deviceType?.attributeTypes?.forEach(attrType => {
+                        const {
+                            value,
+                        } = AttributeValueGet({ attrType, data: d, deviceItem, scope: SCOPE_TRIAL });
+
+                        properties[attrType.name] = value;
+                    });
+
+                    json.features.push({
+                        type: 'Feature',
+                        geometry: {
+                            type: 'Point',
+                            coordinates,
+                        },
+                        properties
+                    });
                 }
-                return {
-                    type: 'Feature',
-                    geometry: {
-                        type: 'Point',
-                        coordinates,
-                    },
-                    properties
-                }
-            })
-        };
+            }
+        }
+
         const filename = `trial_${experiment.name}_${trialType.name}_${data.name}.geojson`;
         const blob = new Blob([JSON.stringify(json, null, 2)], { type: 'application/json' })
         saveAs(blob, filename);
