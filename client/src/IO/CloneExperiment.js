4a2c4eeb9d366a0c3bc8553e1b5bff0d1e6a529f
Eran Geva <eran@kaplanopensource.co.il>
2024-07-09 13:00:54 +0200

    cloneExperiment with images

--- client/src/IO/CloneExperiment.js
+++ client/src/IO/CloneExperiment.js
@@ -8,9 +8,26 @@ export const useCloneExperiment = () => {
     const { experiments, addExperiment } = useContext(experimentContext);
     const { uploadImage, downloadImageAsBlob } = useUploadImage();
 
-    const cloneExperiment = useCallback((exp) => {
-        const name = createNewName(experiments, exp.name + " cloned");
-        addExperiment({ ...deepClone(exp), name });
+    const cloneExperiment = useCallback(async (experiment) => {
+        const name = createNewName(experiments, experiment.name + " cloned");
+        const exp = { ...deepClone(experiment), name };
+
+        const images = (exp.imageStandalone || []).concat((exp.imageEmbedded || []));
+        for (const img of images) {
+            if (img.filename) {
+                const imageBlob = await downloadImageAsBlob(experiment.name, img.filename);
+                console.log(imageBlob, img)
+                const ret = await uploadImage(imageBlob, img.name, name, img.filename);
+                if (ret) {
+                    const { filename, height, width } = ret;
+                    img.filename = filename;
+                    img.height = height;
+                    img.width = width;
+                }
+            }
+        }
+
+        addExperiment(exp);
     }, [experiments]);
 
     return {
