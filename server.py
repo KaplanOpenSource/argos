226ca25d355f7813ef7821fee65940fed59832c7
Eran Geva <eran@kaplanopensource.co.il>
2024-08-06 15:59:06 +0200

    prod server

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta, timezone
 import json
 import os
 from flask import Flask, jsonify, send_from_directory, request
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from flask_jwt_extended import (
     JWTManager,
     create_access_token,
@@ -12,6 +12,10 @@ from flask_jwt_extended import (
     jwt_required,
     unset_jwt_cookies,
 )
+import waitress
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 from py.Images import Images
 from py.Experiments import Experiments
@@ -46,26 +50,27 @@ def index_func():
 
 
 def check_user(username, password):
-    if not args.prod and username == '1' and password == '1':
+    if not args.prod and username == "1" and password == "1":
         return True
-    
+
     try:
         with open(os.path.join(DATA_FOLDER, "users.json")) as file:
             all_users = json.load(file)["users"]
-            curr_user = [a for a in all_users if a['username'] == username]
-            if len(curr_user) != 1 or curr_user[0]['password'] != password:
+            curr_user = [a for a in all_users if a["username"] == username]
+            if len(curr_user) != 1 or curr_user[0]["password"] != password:
                 return "Wrong username or password"
     except:
         return "Problem with login"
-    
+
     return True
 
 
 @app.route("/login", methods=["POST"])
+@cross_origin()
 def create_token():
     username = request.json.get("username", None)
     password = request.json.get("password", None)
-    
+
     res = check_user(username, password)
     if res != True:
         return {"msg": res}, 401
@@ -76,6 +81,7 @@ def create_token():
 
 
 @app.route("/logout", methods=["POST"])
+@cross_origin()
 def logout():
     response = jsonify({"msg": "logout successful"})
     unset_jwt_cookies(response)
@@ -102,18 +108,21 @@ def refresh_expiring_jwts(response):
 
 @app.route("/<path:path>")
 @jwt_required()
+@cross_origin()
 def static_file(path):
     return app.send_static_file(path)
 
 
 @app.route("/experiment_list")
 @jwt_required()
+@cross_origin()
 def experimentListReq():
     return exps.get_list()
 
 
 @app.route("/experiment/<name>")
 @jwt_required()
+@cross_origin()
 def experimentGetReq(name):
     ret = exps.get_exp(name)
     filepath = ret.get("filepath", None)
@@ -124,6 +133,7 @@ def experimentGetReq(name):
 
 @app.route("/experiment_set/<name>", methods=["POST"])
 @jwt_required()
+@cross_origin()
 def experimentSetReq(name):
     json_str = request.get_data()
     return exps.set_exp(name, json_str)
@@ -131,6 +141,7 @@ def experimentSetReq(name):
 
 @app.route("/upload", methods=["POST"])
 @jwt_required()
+@cross_origin()
 def upload():
     # if "file" not in request.files:  # check if the post request has the file part
     #     return {"error": "No file part"}
@@ -145,6 +156,7 @@ def upload():
 
 @app.route("/uploads/<experimentName>/<filename>")
 @jwt_required()
+@cross_origin()
 def download_file(experimentName, filename):
     ret = images.download(experimentName, filename)
     filepath = ret.get("filepath", None)
@@ -155,4 +167,7 @@ def download_file(experimentName, filename):
 
 if __name__ == "__main__":  # pragma: no cover
     print(exps.get_list())
-    app.run(host="0.0.0.0", port=int(args.port))
+    if args.prod:
+        waitress.serve(app, host="0.0.0.0", port=args.port)
+    else:
+        app.run(host="0.0.0.0", port=int(args.port))
