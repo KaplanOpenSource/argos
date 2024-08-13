207059b2fdf6a889ebb4d58230669092698ed39a
Eran Geva <eran@kaplanopensource.co.il>
2024-08-13 17:43:00 +0200

    multi maps on CoordsSpan

--- client/src/Experiment/CoordsSpan.tsx
+++ client/src/Experiment/CoordsSpan.tsx
@@ -4,28 +4,24 @@ import { ICoordinates, IExperiment, ITrial } from "../types/types";
 import { circleToPolygon } from "../IO/ShapesToGeoJson";
 
 export class CoordsSpan {
-    private readonly coords: LatLng[] = [];
+    private readonly coords = new Map<string, LatLng[]>;
 
     public fromExperiment(experiment: IExperiment) {
         for (const x of experiment?.imageEmbedded || []) {
-            if (x?.latsouth && x?.lngwest) {
-                this.coords.push(latLng(x?.latsouth, x?.lngwest));
-            }
-            if (x?.latnorth && x?.lngeast) {
-                this.coords.push(latLng(x?.latnorth, x?.lngeast));
-            }
+            this.addCoord(x?.latsouth, x?.lngwest);
+            this.addCoord(x?.latnorth, x?.lngeast);
         }
 
         for (const trialType of experiment?.trialTypes || []) {
             for (const trial of trialType?.trials || []) {
-                this.fromTrial(trial, RealMapName);
+                this.fromTrial(trial);
             }
         }
 
         for (const shape of experiment?.shapes || []) {
             for (const c of shape?.coordinates || []) {
                 if (c) {
-                    this.coords.push(latLng(c[0], c[1]));
+                    this.addCoord(c[0], c[1]);
                 }
             }
             if (shape?.center && shape?.center.length === 2 && shape?.radius) {
@@ -34,7 +30,7 @@ export class CoordsSpan {
                 const circle = circleToPolygon(center, shape?.radius, 20);
                 for (const c of circle || []) {
                     if (c) {
-                        this.coords.push(latLng(c[0], c[1]));
+                        this.addCoord(c[0], c[1]);
                     }
                 }
             }
@@ -42,23 +38,40 @@ export class CoordsSpan {
         return this;
     }
 
-    public fromTrial(trial: ITrial, mapName: string) {
+    public fromTrial(trial: ITrial) {
         for (const dev of trial?.devicesOnTrial || []) {
-            if (dev?.location?.name === mapName) {
-                const [lat, lng]: ICoordinates | [] = dev?.location?.coordinates || [];
-                if (lat && lng) {
-                    this.coords.push(latLng(lat, lng));
-                }
-            }
+            // if (dev?.location?.name === mapName) {
+            const [lat, lng]: ICoordinates | [] = dev?.location?.coordinates || [];
+            this.addCoord(lat, lng);
+            // }
         }
         return this;
     }
 
-    getBounds() {
-        const good: LatLng[] = this.coords.filter(x => x && isFinite(x.lat) && isFinite(x.lng));
+    getMaps() {
+        if (this.coords.size === 0) {
+            return [RealMapName];
+        }
+        return [...this.coords.keys()];
+    }
+
+    getBounds(mapName: string = RealMapName) {
+        const coordMap = this.coords.get(mapName) || [];
+        const good: LatLng[] = coordMap.filter(x => x && isFinite(x.lat) && isFinite(x.lng));
         if (good.length) {
             return latLngBounds(good);
         }
         return latLngBounds([32.071128, 34.769729], [32.091128, 34.789729]);
+    }
+
+    private addCoord(lat: number | undefined, lng: number | undefined, mapName: string = RealMapName) {
+        if (lat && lng) {
+            let coordMap = this.coords.get(mapName);
+            if (!coordMap) {
+                coordMap = [];
+                this.coords.set(mapName, coordMap);
+            }
+            coordMap.push(latLng(lat, lng));
+        }
     }
 }
