1cbfc7fd5cca9b35255236b471b2481ba3f122b5
Eran Geva <eran@kaplanopensource.co.il>
2024-08-20 17:11:05 +0200

    config.json with tileserver info

--- client/src/Map/RealMap.jsx
+++ client/src/Map/RealMap.jsx
@@ -1,12 +1,45 @@
+import { useEffect, useState } from "react"
 import { TileLayer } from "react-leaflet"
 
 export const RealMap = ({ }) => {
+    const [tileServerInfo, setTileServerInfo] = useState({});
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const resp = await fetch("/config.json");
+                if (resp.ok) {
+                    const com = await resp.json();
+                    if (!com || !com.tileserver || !com.tileserver.url) {
+                        throw "bad config json";
+                    }
+                    setTileServerInfo(com.tileserver);
+                } else {
+                    throw "no config json";
+                }
+            } catch (e) {
+                alert(e);
+            }
+        })();
+    }, []);
+
     return (
-        <TileLayer
-            attribution='&copy; <a href="https://carto.com">Carto</a> contributors'
-            url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}.png'
-        // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+        <>
+            {(tileServerInfo && tileServerInfo.url)
+                ? (
+                    <TileLayer
+                        attribution={tileserver.attribution || ""}
+                        url={tileServerInfo.url}
+
+                    // attribution='&copy; <a href="https://carto.com">Carto</a> contributors'
+                    // url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}.png'
+
+                    // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+
+                    />)
+                : null
+            }
+        </>
     )
 }
