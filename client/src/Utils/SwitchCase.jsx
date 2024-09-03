57b9f1e8b5705b8d54965b210dcea47b3fb3c9c9
Eran Geva <eran@kaplanopensource.co.il>
2024-09-03 18:29:09 +0200

    comment

--- client/src/Utils/SwitchCase.jsx
+++ client/src/Utils/SwitchCase.jsx
@@ -1,3 +1,9 @@
+/**
+ * `SwitchCase` picks one child element and renders only it.  
+ * The chosen child has a prop `value` that is equal to the value of `test` on `SwitchCase`.  
+ * If no child is chosen, pick the first child that has a prop `isDefault={true}`.  
+ * The `Case` element is used to formulate this.  
+ */
 export const SwitchCase = ({ test, children }) => {
     const childrenWrapped = children instanceof Array ? children : [children];
     for (const child of childrenWrapped) {
