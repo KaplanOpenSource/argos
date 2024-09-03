eb98fde413cca7f7fd2a45ded371904fe6eea5da
Eran Geva <eran@kaplanopensource.co.il>
2024-09-03 11:25:54 +0200

    Created date and positioned devices on tabular

--- client/src/Experiment/TrialsTabularView.jsx
+++ client/src/Experiment/TrialsTabularView.jsx
@@ -1,9 +1,12 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip } from "@mui/material"
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip, Typography } from "@mui/material"
 import { AttributeItemOne } from "./AttributeItemList";
 import { SCOPE_TRIAL } from "./AttributeType";
 import { Fragment } from "react";
+import { DateProperty } from "../Property/DateProperty";
+import { sum } from "lodash";
 
 export const TrialsTabularView = ({ data, setData }) => {
+    const totalDevices = sum((data?.deviceTypes || []).map(x => x.devices.length));
     return (
         <TableContainer component={Paper} key="1">
             <Table
@@ -17,6 +20,8 @@ export const TrialsTabularView = ({ data, setData }) => {
                             <TableRow sx={{ backgroundColor: 'lightgray' }}>
                                 <TableCell key={':tt'}>Trial Type</TableCell>
                                 <TableCell key={':tr'}>Trial</TableCell>
+                                <TableCell key={':tcd'}>Created Date</TableCell>
+                                <TableCell key={':tpos'}>Positioned Devices</TableCell>
                                 {trialType?.attributeTypes?.map(attrType => {
                                     let shortName = attrType.name;
                                     if (shortName.length > 20) {
@@ -38,36 +43,52 @@ export const TrialsTabularView = ({ data, setData }) => {
                             </TableRow>
                         </TableHead>
                         <TableBody key={':tb_' + trialType.name}>
-                            {trialType?.trials?.map((trial, itr) => (
-                                <TableRow
-                                    key={trial.trackUuid}
-                                >
-                                    <TableCell component="th" scope="row" key={':tt'}>
-                                        {trialType.name}
-                                    </TableCell>
-                                    <TableCell key={':tr'}>{trial.name}</TableCell>
-                                    {trialType?.attributeTypes?.map(attrType => {
-                                        return (
-                                            <TableCell
-                                                key={attrType.name}
-                                            >
-                                                <AttributeItemOne
-                                                    attrType={attrType}
-                                                    data={trial}
-                                                    setData={val => {
-                                                        const exp = structuredClone(data);
-                                                        exp.trialTypes[itt].trials[itr] = val;
-                                                        setData(exp);
-                                                    }}
-                                                    scope={SCOPE_TRIAL}
-                                                    reduceNames={true}
-                                                />
-                                            </TableCell>
-                                        )
-                                    })}
-                                </TableRow>
+                            {trialType?.trials?.map((trial, itr) => {
+                                const placedDevices = (trial.devicesOnTrial || []).length;
+                                return (
+                                    <TableRow
+                                        key={trial.trackUuid}
+                                    >
+                                        <TableCell component="th" scope="row" key={':tt'}>
+                                            {trialType.name}
+                                        </TableCell>
+                                        <TableCell key={':tr'}>{trial.name}</TableCell>
+                                        <TableCell key={':tcd'}>
+                                            <DateProperty
+                                                data={trial.createdDate}
+                                                // setData={val => setData({ ...data, createdDate: val })}
+                                                label="Created Date"
+                                                disabled={true}
+                                            />
+                                        </TableCell>
+                                        <TableCell key={':tpos'}>
+                                            <Typography>
+                                                {placedDevices}/{totalDevices}
+                                            </Typography>
+                                        </TableCell>
+                                        {trialType?.attributeTypes?.map(attrType => {
+                                            return (
+                                                <TableCell
+                                                    key={attrType.name}
+                                                >
+                                                    <AttributeItemOne
+                                                        attrType={attrType}
+                                                        data={trial}
+                                                        setData={val => {
+                                                            const exp = structuredClone(data);
+                                                            exp.trialTypes[itt].trials[itr] = val;
+                                                            setData(exp);
+                                                        }}
+                                                        scope={SCOPE_TRIAL}
+                                                        reduceNames={true}
+                                                    />
+                                                </TableCell>
+                                            )
+                                        })}
+                                    </TableRow>
 
-                            ))}
+                                )
+                            })}
                         </TableBody>
                     </Fragment>
                 ))}
