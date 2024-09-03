e344376210e43026a498ca013292115b6198e9a1
Eran Geva <eran@kaplanopensource.co.il>
2024-09-03 09:05:31 +0200

    show attributes on trial table

--- client/src/Experiment/AttributeItemList.jsx
+++ client/src/Experiment/AttributeItemList.jsx
@@ -1,35 +1,52 @@
 import { AttributeValue } from "./AttributeValue";
 import { AttributeValueGet } from "./AttributeValueGet";
 
-export const AttributeItemList = ({ attributeTypes, data, setData, scope, deviceItem }) => {
+export const AttributeItemList = ({
+    attributeTypes, // Type of the attributes from the container's type
+    data, // data of the container (device / trial)
+    setData, // set the container
+    deviceItem, // device item when applicable
+    scope, // scope of the using component     
+}) => {
+    return (attributeTypes || []).map(attrType => (
+        <AttributeItemOne
+            key={attrType.name}
+            attrType={attrType}
+            data={data}
+            setData={setData}
+            scope={scope}
+            deviceItem={deviceItem}
+        />
+    ))
+}
+
+export const AttributeItemOne = ({
+    attrType, // Type of the attribute from the container's type
+    data, // data of the container (device / trial)
+    setData, // set the container
+    deviceItem, // device item when applicable
+    scope, // scope of the using component     
+}) => {
+    const {
+        value,
+        setValue,
+        attrTypeScope,
+        attrValueType,
+        editable,
+    } = AttributeValueGet({ attrType, data, setData, deviceItem, scope });
+    const tooltipTitle = editable
+        ? `Attribute "${attrType.name}" can be updated here on ${attrTypeScope} level`
+        : `Attribute "${attrType.name}" can be updated only on ${attrTypeScope} level (this is the ${scope} level)`;
     return (
-        <>
-            {
-                (attributeTypes || []).map(attrType => {
-                    const {
-                        value,
-                        setValue,
-                        attrTypeScope,
-                        attrValueType,
-                        editable,
-                    } = AttributeValueGet({ attrType, data, setData, deviceItem, scope });
-                    const tooltipTitle = editable
-                        ? `Attribute "${attrType.name}" can be updated here on ${attrTypeScope} level`
-                        : `Attribute "${attrType.name}" can be updated only on ${attrTypeScope} level (this is the ${scope} level)`;
-                    return (
-                        <AttributeValue
-                            key={attrType.name}
-                            label={attrType.name}
-                            type={attrValueType}
-                            data={value}
-                            setData={setValue}
-                            attrType={attrType}
-                            disabled={!editable}
-                            tooltipTitle={tooltipTitle}
-                        />
-                    )
-                })
-            }
-        </>
+        <AttributeValue
+            key={attrType.name}
+            label={attrType.name}
+            type={attrValueType}
+            data={value}
+            setData={setValue}
+            attrType={attrType}
+            disabled={!editable}
+            tooltipTitle={tooltipTitle}
+        />
     )
 }
--- client/src/Experiment/TrialsTabularView.jsx
+++ client/src/Experiment/TrialsTabularView.jsx
@@ -1,39 +1,56 @@
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
+import { AttributeItemOne } from "./AttributeItemList";
+import { SCOPE_TRIAL } from "./AttributeType";
+import { Fragment } from "react";
 
 export const TrialsTabularView = ({ data, setData }) => {
-    console.log(data)
     return (
-        <TableContainer component={Paper}>
+        <TableContainer component={Paper} key="1">
             <Table
                 sx={{ minWidth: 650 }}
                 aria-label="simple table"
                 size="small"
             >
                 {data?.trialTypes?.map((trialType) => (
-                    <>
-                        <TableHead>
+                    <Fragment key={trialType.trackUuid}>
+                        <TableHead key={':th_' + trialType.name}>
                             <TableRow sx={{ backgroundColor: 'lightgray' }}>
-                                <TableCell>Trial Type</TableCell>
-                                <TableCell>Trial</TableCell>
+                                <TableCell key={':tt'}>Trial Type</TableCell>
+                                <TableCell key={':tr'}>Trial</TableCell>
                                 {trialType?.attributeTypes?.map(attrType => (
-                                    <TableCell>{attrType.name}</TableCell>
+                                    <TableCell
+                                        key={attrType.name}
+                                    >
+                                        {attrType.name}
+                                    </TableCell>
                                 ))}
                             </TableRow>
                         </TableHead>
-                        <TableBody>
+                        <TableBody key={':tb_' + trialType.name}>
                             {trialType?.trials?.map((trial) => (
                                 <TableRow
                                     key={trial.trackUuid}
                                 >
-                                    <TableCell component="th" scope="row">
+                                    <TableCell component="th" scope="row" key={':tt'}>
                                         {trialType.name}
                                     </TableCell>
-                                    <TableCell >{trial.name}</TableCell>
+                                    <TableCell key={':tr'}>{trial.name}</TableCell>
                                     {trialType?.attributeTypes?.map(attrType => {
                                         const attr = trial?.attributes?.find(x => x.name === attrType.name);
                                         return attr
                                             ? (
-                                                <TableCell>{JSON.stringify(attr.value)}</TableCell>
+                                                <TableCell
+                                                    key={attrType.name}
+                                                >
+                                                    <AttributeItemOne
+                                                        attrType={attrType}
+                                                        data={trial}
+                                                        // setData={setData}
+                                                        scope={SCOPE_TRIAL}
+                                                    // deviceItem={deviceItem}
+                                                    />
+                                                    {/* {JSON.stringify(attr.value)} */}
+                                                </TableCell>
                                             )
                                             : null;
                                     })}
@@ -41,7 +58,7 @@ export const TrialsTabularView = ({ data, setData }) => {
 
                             ))}
                         </TableBody>
-                    </>
+                    </Fragment>
                 ))}
             </Table>
         </TableContainer>
