e70827e2daeb98c8d6a71133f5ee245ee7250209
Eran Geva <eran@kaplanopensource.co.il>
2024-10-02 17:47:44 +0200

    collapse device type on tabular

--- client/src/Experiment/DevicesTabularOneType.jsx
+++ client/src/Experiment/DevicesTabularOneType.jsx
@@ -1,4 +1,4 @@
-import { TableBody, TableCell, TableHead, TableRow, Tooltip } from "@mui/material"
+import { Paper, TableBody, TableCell, TableHead, TableRow, Tooltip } from "@mui/material"
 import { AttributeItemOne } from "./AttributeItemList";
 import { SCOPE_EXPERIMENT } from "./AttributeType";
 import { AttributeTypesDialogButton } from "./AttributeTypesDialogButton";
@@ -7,8 +7,10 @@ import { createNewName } from "../Utils/utils";
 import { Add } from "@mui/icons-material";
 import { AddMultipleDevices } from "./AddMultipleDevices";
 import { assignUuids } from "../Context/TrackUuidUtils";
+import { useState } from "react";
 
 export const DevicesTabularOneType = ({ deviceType, setDeviceType }) => {
+    const [open, setOpen] = useState(true);
     return (
         <>
             <TableHead key={':th_' + deviceType.name}>
@@ -17,7 +19,9 @@ export const DevicesTabularOneType = ({ deviceType, setDeviceType }) => {
                         backgroundColor: 'lightgray'
                     }}
                 >
-                    <TableCell key={':tt'}>
+                    <TableCell key={':tt'}
+                        onClick={() => setOpen(!open)}
+                    >
                         Device Type
                         <AttributeTypesDialogButton
                             data={deviceType}
@@ -65,38 +69,64 @@ export const DevicesTabularOneType = ({ deviceType, setDeviceType }) => {
                 </TableRow>
             </TableHead>
             <TableBody key={':tb_' + deviceType.name}>
-                {deviceType?.devices?.map((device, itr) => {
-                    const setDevice = (val) => {
-                        const t = structuredClone(deviceType);
-                        t.devices[itr] = val;
-                        setDeviceType(t);
-                    }
-                    return (
-                        <TableRow
-                            key={device.trackUuid}
-                        >
-                            <TableCell component="th" scope="row" key={':tt'}>
-                                {deviceType.name}
-                            </TableCell>
-                            <TableCell key={':tr'}>{device.name}</TableCell>
-                            {deviceType?.attributeTypes?.map(attrType => {
+                {open
+                    ? (
+                        <>
+                            {deviceType?.devices?.map((device, itr) => {
+                                const setDevice = (val) => {
+                                    const t = structuredClone(deviceType);
+                                    t.devices[itr] = val;
+                                    setDeviceType(t);
+                                }
                                 return (
-                                    <TableCell
-                                        key={attrType.name}
+                                    <TableRow
+                                        key={device.trackUuid}
                                     >
-                                        <AttributeItemOne
-                                            attrType={attrType}
-                                            data={device}
-                                            setData={val => setDevice(val)}
-                                            scope={SCOPE_EXPERIMENT}
-                                            reduceNames={true}
-                                        />
-                                    </TableCell>
+                                        <TableCell component="th" scope="row" key={':tt'}>
+                                            {deviceType.name}
+                                        </TableCell>
+                                        <TableCell key={':tr'}>{device.name}</TableCell>
+                                        {deviceType?.attributeTypes?.map(attrType => {
+                                            return (
+                                                <TableCell
+                                                    key={attrType.name}
+                                                >
+                                                    <AttributeItemOne
+                                                        attrType={attrType}
+                                                        data={device}
+                                                        setData={val => setDevice(val)}
+                                                        scope={SCOPE_EXPERIMENT}
+                                                        reduceNames={true}
+                                                    />
+                                                </TableCell>
+                                            )
+                                        })}
+                                    </TableRow>
                                 )
                             })}
+                        </>
+                    )
+                    : (
+                        <TableRow
+                            style={{
+                                backgroundSize: "10px 10px",
+                                background: "repeating-linear-gradient(-45deg,#fff,#fff 10px,#eee 10px,#eee 20px)",
+                            }}
+                            onClick={() => setOpen(true)}
+                        >
+                            <TableCell component="th" scope="row" key={'name'}>
+                                {deviceType.name}
+                            </TableCell>
+                            <TableCell component="th" scope="row" key={'num'}>
+                                {deviceType?.devices?.length || 0} devices (collapsed)
+                            </TableCell>
+                            <TableCell key={'stripes'}
+                                colSpan={'100%'}
+                            >
+                            </TableCell>
                         </TableRow>
                     )
-                })}
+                }
             </TableBody>
         </>
     )
