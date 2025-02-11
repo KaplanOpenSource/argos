8823e5362b440771e48c33847e36d67577f986b1
Eran Geva <eran@kaplanopensource.co.il>
2025-02-11 13:39:54 +0200

    downloadImageAsBlob types

--- client/src/IO/UploadImage.ts
+++ client/src/IO/UploadImage.ts
@@ -81,24 +81,42 @@ export const useUploadImage = () => {
         })
     }
 
-    const downloadImageAsUrl = useCallback(async (experimentName, dataFilename) => {
+    const downloadImageAsUrl = useCallback(async (
+        experimentName: string,
+        dataFilename: string
+    ): Promise<string | undefined> => {
         if (!isLoggedIn()) {
+            console.error('not logged in')
             return undefined;
         }
-        const url = "uploads/" + experimentName + "/" + dataFilename;
-        const resp = await axiosSecure().get(url, { responseType: "arraybuffer" });
-        const converted = new Uint8Array(resp.data).reduce((data, byte) => data + String.fromCharCode(byte), '');
-        const base64 = `data:image/png;base64, ` + btoa(converted);
-        return base64;
+        try {
+            const url = "uploads/" + experimentName + "/" + dataFilename;
+            const resp = await axiosSecure().get(url, { responseType: "arraybuffer" });
+            const converted = new Uint8Array(resp.data).reduce((data, byte) => data + String.fromCharCode(byte), '');
+            const base64 = `data:image/png;base64, ` + btoa(converted);
+            return base64;
+        } catch (e) {
+            console.error('download error:', e)
+            return undefined;
+        }
     }, [axiosSecure]);
 
-    const downloadImageAsBlob = useCallback(async (experimentName, dataFilename) => {
+    const downloadImageAsBlob = useCallback(async (
+        experimentName: string,
+        dataFilename: string,
+    ): Promise<Blob | undefined> => {
         if (!isLoggedIn()) {
+            console.error('not logged in')
             return undefined;
         }
-        const url = "uploads/" + experimentName + "/" + dataFilename;
-        const resp = await axiosSecure().get(url, { responseType: "blob" });
-        return resp.data;
+        try {
+            const url = "uploads/" + experimentName + "/" + dataFilename;
+            const resp = await axiosSecure().get(url, { responseType: "blob" });
+            return resp.data;
+        } catch (e) {
+            console.error('download error:', e)
+            return undefined;
+        }
     }, [axiosSecure]);
 
     return {
