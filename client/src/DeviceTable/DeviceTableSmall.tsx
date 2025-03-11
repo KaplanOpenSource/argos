04f6627d8664b3014fd30172bbfacdc9b89c17c4
Eran Geva <eran@kaplanopensource.co.il>
2025-03-11 17:35:02 +0000

    cleanup

--- client/src/DeviceTable/DeviceTableSmall.tsx
+++ client/src/DeviceTable/DeviceTableSmall.tsx
@@ -1,48 +1,13 @@
-import React, { useContext, useState } from "react";
-import { Paper, Stack, Typography } from "@mui/material"
-import { SelectDeviceButton } from "../Experiment/SelectDeviceButton";
-import { DeviceItemLocationButton } from "../Experiment/DeviceItemLocationButton";
-import { IDevice, IDeviceType, IDeviceTypeAndItem, IExperiment, ITrackUuid } from "../types/types";
+import React, { useContext } from "react";
+import { Stack } from "@mui/material"
+import { IDevice, IDeviceTypeAndItem, IExperiment, ITrackUuid } from "../types/types";
 import { useDeviceSeletion } from "../Context/useDeviceSeletion";
 import { experimentContext } from "../Context/ExperimentProvider";
 
-import { DndContext, closestCenter } from '@dnd-kit/core';
-import { arrayMove, SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
 import { SortableList } from "../lib/SortableList/SortableList";
 import { DeviceSmallClip, ISelectedIndexedItem } from "./DeviceSmallClip";
 
-// const { DndContext, closestCenter } = window['dnd-kit'];
-//     const { arrayMove, SortableContext, verticalListSortingStrategy } = window.sortable;
-// const { useState } = React;
-
-// const Comp = ({ items, setItems }) => {
-//     return (
-//         <SortableList
-//             items={items}
-//             onChange={setItems}
-//             renderItem={(item) => (
-//                 <SortableList.Item id={item.id}>
-//                     {item.id}
-//                     <SortableList.DragHandle />
-//                 </SortableList.Item>
-//             )}
-//         />
-//     );
-// };
-
-// const items = [
-//     { id: '1', name: 'Item 1' },
-//     { id: '2', name: 'Item 2' },
-//     { id: '3', name: 'Item 3' },
-//     { id: '4', name: 'Item 4' },
-//     { id: '5', name: 'Item 5' },
-// ];
-
 export const DeviceTableSmall = ({ }) => {
-    //     return <Paper>
-    //         <Comp items={appItems} setItems={setAppItems} />
-    //     </Paper>
-    // }
 
     const { selection, setSelection } = useDeviceSeletion();
     const { currTrial } = useContext(experimentContext);
@@ -57,9 +22,6 @@ export const DeviceTableSmall = ({ }) => {
             shownDevices.push({ deviceType, deviceItem, deviceTypeName, deviceItemName, id });
         }
     };
-
-    // const [items, setItems] = useState(shownDevices.map((x, i) => ({ ...x, id: x.deviceItem.trackUuid || i.toString() })));
-    // const items = shownDevices.map((x, i) => ({ ...x, id: x.deviceItem.trackUuid || i.toString() }));
 
     return (
         <SortableList
@@ -84,21 +46,5 @@ export const DeviceTableSmall = ({ }) => {
             )}
         />
     );
-
-    // return (
-    //     <Stack
-    //         direction='column'
-    //         alignItems="flex-end"
-    //     >
-    //         {shownDevices.map(({ deviceType, deviceItem }) => (
-    //             <DeviceSmallClip
-    //                 key={deviceItem.trackUuid}
-    //                 deviceItem={deviceItem}
-    //                 deviceType={deviceType}
-    //                 shownDevices={shownDevices}
-    //             />
-    //         ))}
-    //     </Stack>
-    // )
 }
 
