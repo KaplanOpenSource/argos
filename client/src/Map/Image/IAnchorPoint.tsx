4c51cf1aa930dec925210e768022509b3abbfc77
Eran Geva <eran@kaplanopensource.co.il>
2025-03-11 09:54:08 +0000

    move ComputedImageData

--- client/src/Map/Image/IAnchorPoint.tsx
+++ client/src/Map/Image/IAnchorPoint.tsx
@@ -1,6 +1,10 @@
 export interface IAnchorPoint {
-    lat: number;
+    /** Latitude in meters on the map */
+    lat: number; 
+    /** Longitude in meters on the map */
     lng: number;
+    /** X in pixels on the image */
     x: number;
+    /** Y in pixels on the image */
     y: number;
 }
--- client/src/Map/Image/ImagePlacementEditor.tsx
+++ client/src/Map/Image/ImagePlacementEditor.tsx
@@ -10,44 +10,7 @@ import { MarkedPoint } from "../MarkedPoint";
 import { experimentContext } from "../../Context/ExperimentProvider";
 import { IAnchorPoint } from "./IAnchorPoint";
 import { ImagePointEdit } from "./ImagePointEdit";
-
-type IImageBounds = Required<Omit<IImageStandalone, 'name' | 'filename'>>;
-class ComputedImageData {
-    public readonly data: IImageBounds;
-    private readonly xspan: number;
-    private readonly yspan: number;
-
-    constructor(imageData: IImageStandalone) {
-        this.data = {
-            xleft: imageData.xleft ?? 0,
-            ybottom: imageData.ybottom ?? 0,
-            xright: imageData.xright ?? 400,
-            ytop: imageData.ytop ?? 300,
-            height: imageData.height ?? 300,
-            width: imageData.width ?? 400,
-        }
-        this.xspan = this.data.xright - this.data.xleft;
-        this.yspan = this.data.ytop - this.data.ybottom;
-    }
-    public calcXY = ({ lat, lng }: { lat: number, lng: number }): IAnchorPoint => {
-        return ({ lat, lng, x: this.calcX(lng), y: this.calcY(lat) });
-    }
-    public calcLatLng = ({ x, y }: { x: number, y: number }): IAnchorPoint => {
-        return { x, y, lat: this.calcLat(y), lng: this.calcLng(x) };
-    }
-    public calcLat(y: number) {
-        return y * this.yspan / this.data.height + this.data.ybottom;
-    }
-    public calcLng(x: number) {
-        return x * this.xspan / this.data.width + this.data.xleft;
-    }
-    public calcY(lat: number) {
-        return (lat - this.data.ybottom) / this.yspan * this.data.height;
-    }
-    public calcX(lng: number) {
-        return (lng - this.data.xleft) / this.xspan * this.data.width;
-    }
-};
+import { ComputedImageData } from "./ComputedImageData";
 
 export const ImagePlacementEditor = ({
     imageData,
