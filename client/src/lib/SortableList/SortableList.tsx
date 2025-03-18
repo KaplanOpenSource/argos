15edccabdcc6749d9b4196cb5c2c87a8d0dbcdc7
Eran Geva <eran@kaplanopensource.co.il>
2025-03-18 12:18:01 +0000

    range click selection on selection with shift key

--- client/src/lib/SortableList/SortableList.tsx
+++ client/src/lib/SortableList/SortableList.tsx
@@ -1,17 +1,17 @@
 import React, { useMemo, useState } from "react";
 import type { ReactNode } from "react";
 import {
-  DndContext,
-  KeyboardSensor,
-  PointerSensor,
-  useSensor,
-  useSensors
+    DndContext,
+    KeyboardSensor,
+    PointerSensor,
+    useSensor,
+    useSensors
 } from "@dnd-kit/core";
 import type { Active, UniqueIdentifier } from "@dnd-kit/core";
 import {
-  SortableContext,
-  arrayMove,
-  sortableKeyboardCoordinates
+    SortableContext,
+    arrayMove,
+    sortableKeyboardCoordinates
 } from "@dnd-kit/sortable";
 
 import { SortableOverlay } from "./SortableOverlay";
@@ -19,87 +19,108 @@ import { DragHandle, SortableItem } from "./SortableItem";
 import { Box } from "@mui/material";
 
 interface BaseItem {
-  id: UniqueIdentifier;
+    id: UniqueIdentifier;
 }
 
 interface Props<T extends BaseItem> {
-  items: T[];
-  onChange(items: T[]): void;
-  renderItem(item: T, isSelected: boolean): ReactNode;
+    items: T[];
+    onChange(items: T[]): void;
+    renderItem(item: T, isSelected: boolean): ReactNode;
 }
 
 export function SortableList<T extends BaseItem>({
-  items,
-  onChange,
-  renderItem
+    items,
+    onChange,
+    renderItem
 }: Props<T>) {
-  const [active, setActive] = useState<Active | null>(null);
-  const activeItem = useMemo(
-    () => items.find((item) => item.id === active?.id),
-    [active, items]
-  );
-  const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates
-    })
-  );
+    const [active, setActive] = useState<Active | null>(null);
+    const activeItem = useMemo(
+        () => items.find((item) => item.id === active?.id),
+        [active, items]
+    );
+    const sensors = useSensors(
+        useSensor(PointerSensor),
+        useSensor(KeyboardSensor, {
+            coordinateGetter: sortableKeyboardCoordinates
+        })
+    );
 
-  const [selectedItems, setSelectedItems] = useState<UniqueIdentifier[]>([]);
+    const [selectedItems, setSelectedItems] = useState<UniqueIdentifier[]>([]);
+    const [lastSelectedClick, setLastSelectedClick] = useState<UniqueIdentifier | null>(null);
 
-  return (
-    <DndContext
-      sensors={sensors}
-      onDragStart={({ active }) => {
-        setActive(active);
-      }}
-      onDragEnd={({ active, over }) => {
-          if (over && (active?.id !== over?.id || selectedItems.length > 0)) {
-            const next = [...items];
-            const overIndex = next.findIndex(({ id }) => id === over.id);
-            const activeIndexBefore = next.findIndex(({ id }) => id === active.id);
-            next.splice(overIndex, 0, ...next.splice(activeIndexBefore, 1));
-            const idms = selectedItems.filter(x => x !== active.id);
-            const moved = next.filter(x => idms.includes(x.id));
-            const stay = next.filter(x => !idms.includes(x.id));
-            const activeIndexAfter = stay.findIndex(({ id }) => id === active.id);
-            stay.splice(activeIndexAfter + 1, 0, ...moved);
-            onChange(stay);
-        }
-        setActive(null);
-      }}
-      onDragCancel={() => {
-        setActive(null);
-      }}
-    >
-      <SortableContext items={items}>
-        <Box 
-            role="application"
+    const betweenIds = (id0: UniqueIdentifier, id1: UniqueIdentifier): UniqueIdentifier[] => {
+        const i0 = items.findIndex(x => x.id === id0);
+        const i1 = items.findIndex(x => x.id === id1);
+        if (Math.min(i0, i1) <= -1) return [];
+        return items.slice(Math.min(i0, i1), Math.max(i0, i1) + 1).map(x => x.id);
+    }
+
+    return (
+        <DndContext
+            sensors={sensors}
+            onDragStart={({ active }) => {
+                setActive(active);
+            }}
+            onDragEnd={({ active, over }) => {
+                if (over && (active?.id !== over?.id || selectedItems.length > 0)) {
+                    const next = [...items];
+                    const overIndex = next.findIndex(({ id }) => id === over.id);
+                    const activeIndexBefore = next.findIndex(({ id }) => id === active.id);
+                    next.splice(overIndex, 0, ...next.splice(activeIndexBefore, 1));
+                    const idms = selectedItems.filter(x => x !== active.id);
+                    const moved = next.filter(x => idms.includes(x.id));
+                    const stay = next.filter(x => !idms.includes(x.id));
+                    const activeIndexAfter = stay.findIndex(({ id }) => id === active.id);
+                    stay.splice(activeIndexAfter + 1, 0, ...moved);
+                    onChange(stay);
+                }
+                setActive(null);
+            }}
+            onDragCancel={() => {
+                setActive(null);
+            }}
         >
-          {items.map((item) => (
-            <Box key={item.id} 
-                onClick={e => {
-                    if (selectedItems.includes(item.id)) {
-                        setSelectedItems(selectedItems.filter(x => x !== item.id));
-                    } else {
-                        setSelectedItems([...selectedItems, item.id]);
-                    }
-                }}
-            >
-                {renderItem(item, selectedItems.includes(item.id))}
-            </Box>
-          ))}
-        </Box>
-      </SortableContext>
-      <SortableOverlay>
-        {activeItem
-            ? (
-                renderItem(activeItem, selectedItems.includes(activeItem.id))
-            )
-            : null}
-      </SortableOverlay>
-    </DndContext>
-  );
+            <SortableContext items={items}>
+                <Box role="application">
+                    {items.map((item) => {
+                        const isSelected = selectedItems.includes(item.id);
+                        return (
+                            <Box key={item.id}
+                                onClick={e => {
+                                    console.log(e.shiftKey, lastSelectedClick, item.id)
+                                    if (e.shiftKey && lastSelectedClick) {
+                                        const range = betweenIds(lastSelectedClick, item.id);
+                                        const selectedWithout = selectedItems.filter(x => !range.includes(x));
+                                        if (selectedItems.includes(lastSelectedClick)) {
+                                            setSelectedItems([...selectedWithout, ...range]);
+                                        } else {
+                                            setSelectedItems(selectedWithout);
+                                        }
+                                    } else {
+                                        if (selectedItems.includes(item.id)) {
+                                            setSelectedItems(selectedItems.filter(x => x !== item.id));
+                                        } else {
+                                            setSelectedItems([...selectedItems, item.id]);
+                                        }
+                                    }
+                                    setLastSelectedClick(item.id);
+                                }}
+                            >
+                                {renderItem(item, isSelected)}
+                            </Box>
+                        )
+                    })}
+                </Box>
+            </SortableContext>
+            <SortableOverlay>
+                {activeItem
+                    ? (
+                        renderItem(activeItem, selectedItems.includes(activeItem.id))
+                    )
+                    : null}
+            </SortableOverlay>
+        </DndContext>
+    );
 }
 
 SortableList.Item = SortableItem;
