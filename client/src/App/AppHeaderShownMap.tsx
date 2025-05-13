bc6ca54e935d716d48d0bf165751d6bace47bec3
Eran Geva <eran@kaplanopensource.co.il>
2025-05-13 12:09:55 +0300

    click on shown map name to open maps menu

--- client/src/App/AppHeaderShownMap.tsx
+++ client/src/App/AppHeaderShownMap.tsx
@@ -1,16 +1,18 @@
 import MapIcon from '@mui/icons-material/Map';
 import PublicIcon from '@mui/icons-material/Public';
 import { Stack, Tooltip, Typography } from "@mui/material";
-import React from "react";
+import { useState } from "react";
 import { useExperimentProvider } from "../Context/ExperimentProvider";
 import { useShownMap } from "../Context/useShownMap";
-import { ButtonMenu } from "../Utils/ButtonMenu";
 import { IImageStandalone } from "../types/types";
+import { ButtonTooltip } from '../Utils/ButtonTooltip';
+import { MenuActions } from '../Utils/MenuActions';
 
 export const AppHeaderShownMap = ({ }) => {
   const { currTrial } = useExperimentProvider();
   const { shownMapName, experiment } = currTrial;
   const { switchToMap } = useShownMap({});
+  const [anchorEl, setAnchorEl] = useState<Element | null>(null);
 
   const standaloneNames: string[] = (experiment?.imageStandalone || []).map((x: IImageStandalone) => x.name!);
   const menuItems = standaloneNames.map(name => ({
@@ -30,20 +32,28 @@ export const AppHeaderShownMap = ({ }) => {
       justifyContent="flex-end"
       alignItems="center"
     >
-      <ButtonMenu
+      <ButtonTooltip
         tooltip={(shownMapName ? "Shown map" : realMapText) + ", click to change"}
         color={'inherit'}
-        menuItems={menuItems}
+        onClick={(e: MouseEvent) => setAnchorEl(e.currentTarget as (Element | null))}
       >
         {shownMapName ? <MapIcon /> : <PublicIcon />}
-      </ButtonMenu>
+      </ButtonTooltip>
       {shownMapName && (
-        <Tooltip title={"Name of the map shown"}>
-          <Typography variant="body1" paddingRight={1}>
+        <Tooltip title={"Name of the map shown, click to change map"}>
+          <Typography variant="body1" paddingRight={1}
+            onClick={(e) => setAnchorEl(e.currentTarget as (Element | null))}
+            style={{ cursor: 'pointer' }}
+          >
             {shownMapName}
           </Typography>
         </Tooltip>
       )}
+      <MenuActions
+        anchorEl={anchorEl}
+        setAnchorEl={setAnchorEl}
+        menuItems={menuItems}
+      />
     </Stack>
   )
 }
