caa960fb84f7ca6ef500707402f4b48d5a1d56ea
Eran Geva <eran@kaplanopensource.co.il>
2025-07-10 18:41:24 +0300

    deleteDevice by setExperiment

--- client/src/Experiment/DeviceItem.tsx
+++ client/src/Experiment/DeviceItem.tsx
@@ -1,17 +1,19 @@
 import DeleteIcon from '@mui/icons-material/Delete';
 import { IconButton } from "@mui/material";
 import { TreeRowOnChosen } from "../App/TreeRowOnChosen";
-import { useExperimentProvider } from "../Context/ExperimentProvider";
+import { useChosenTrial } from '../Context/useChosenTrial';
 import { useCurrTrial } from "../Context/useCurrTrial";
+import { useExperiments } from '../Context/useExperiments';
+import { ExperimentObj } from '../objects';
 import { ScopeEnum } from "../types/types";
 import { AttributeItemList } from "./AttributeItemList";
 import { DeviceItemLocationButton } from "./DeviceItemLocationButton";
 import { SelectDeviceButton } from "./SelectDeviceButton";
 
 export const DeviceItem = ({ data, setData, deviceType, showAttributes, devicesEnclosingList, scope, experiment }) => {
-  const { currTrial, deleteDevice } = useExperimentProvider();
-
   const { trial } = useCurrTrial({});
+  const { setExperiment } = useExperiments();
+  const { isExperimentChosen } = useChosenTrial();
   const device = trial.getDevice(deviceType.name, data.name);
 
   return (
@@ -27,7 +29,14 @@ export const DeviceItem = ({ data, setData, deviceType, showAttributes, devicesE
           {setData && experiment &&
             <IconButton
               size="small"
-              onClick={() => deleteDevice({ experimentName: experiment.name, deviceTypeName: deviceType.name, deviceItemName: data.name })}
+              onClick={() => {
+                const exp = new ExperimentObj(experiment);
+                const devtype = exp.deviceTypes.find(dt => dt.name === deviceType.name);
+                if (devtype) {
+                  devtype.devices = devtype.devices.filter(d => d.name !== data.name);
+                  setExperiment(experiment.name, exp.toJson(true));
+                }
+              }}
             >
               <DeleteIcon />
             </IconButton>
@@ -41,7 +50,7 @@ export const DeviceItem = ({ data, setData, deviceType, showAttributes, devicesE
         </>
       }
     >
-      {currTrial.experiment && showAttributes &&
+      {isExperimentChosen() && showAttributes &&
         <AttributeItemList
           attributeTypes={deviceType.attributeTypes}
           data={scope === ScopeEnum.SCOPE_TRIAL ? device.onTrial() : data}
