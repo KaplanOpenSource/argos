81d2431f4f8e2229d934cb71aa81ebe38adf3ef1
Eran Geva <eran@kaplanopensource.co.il>
2025-08-21 13:09:33 +0200

    useExperimentTreeNodesExpandedStore convert to zustand

--- client/src/Experiment/ExperimentTreeNodesExpandedProvider.tsx
+++ client/src/Experiment/ExperimentTreeNodesExpandedProvider.tsx
@@ -1,35 +1,32 @@
-import { createContext, useState } from "react";
+// src/store/experimentTreeNodesExpandedStore.ts
+import { create } from 'zustand';
 
-type IExperimentTreeNodesExpandedStore = {
-  expandedNodes: string[],
-  setExpandedNodes: (nodeNames: string[]) => void,
-  addExpandedNode: (nodeName: string) => void,
-  removeExpandedNode: (nodeName: string) => void,
-};
-
-export const ExperimentTreeNodesExpandedContext = createContext<IExperimentTreeNodesExpandedStore | null>(null);
-
-export const ExperimentTreeNodesExpandedProvider = ({ children }) => {
-  const [expandedNodes, setExpandedNodes] = useState<string[]>([]);
-
-  const addExpandedNode = (newNode: string) => {
-    setExpandedNodes(prev => [...prev.filter(x => x !== newNode), newNode]);
-  };
-
-  const removeExpandedNode = (newNode: string) => {
-    setExpandedNodes(prev => prev.filter(x => x !== newNode));
-  };
-
-  return (
-    <ExperimentTreeNodesExpandedContext.Provider
-      value={{
-        expandedNodes,
-        setExpandedNodes,
-        addExpandedNode,
-        removeExpandedNode,
-      }}
-    >
-      {children}
-    </ExperimentTreeNodesExpandedContext.Provider>
-  )
+interface ExperimentTreeNodesExpandedStore {
+  expandedNodes: string[];
+  setExpandedNodes: (nodeNames: string[]) => void;
+  addExpandedNode: (nodeName: string) => void;
+  removeExpandedNode: (nodeName: string) => void;
 }
+
+export const useExperimentTreeNodesExpandedStore = create<ExperimentTreeNodesExpandedStore>((set) => ({
+  expandedNodes: [],
+
+  setExpandedNodes: (nodeNames) => {
+    set(() => ({ expandedNodes: [...nodeNames] }));
+  },
+
+  addExpandedNode: (nodeName) => {
+    set((prev) => ({
+      expandedNodes: [
+        ...prev.expandedNodes.filter((x) => x !== nodeName),
+        nodeName,
+      ],
+    }));
+  },
+
+  removeExpandedNode: (nodeName) => {
+    set((prev) => ({
+      expandedNodes: prev.expandedNodes.filter((x) => x !== nodeName),
+    }));
+  },
+}));
