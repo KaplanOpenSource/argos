3a94b213d4bcc85f838b5a94033d380774b7e1cb
Eran Geva <eran@kaplanopensource.co.il>
2025-08-26 17:55:00 +0200

    cleanup

--- client/src/Map/Device/SingleDevicePropertiesView.tsx
+++ client/src/Map/Device/SingleDevicePropertiesView.tsx
@@ -3,13 +3,11 @@ import {
   Stack, Typography
 } from '@mui/material';
 import { useChosenTrial } from '../../Context/useChosenTrial';
-import { useExperiments } from '../../Context/useExperiments';
 import { AttributeValue } from '../../Experiment/AttributeValue';
 import { AddContainedButton } from '../../Experiment/Contained/AddContainedButton';
 import { DeviceItemLocationButton } from '../../Experiment/DeviceItemLocationButton';
 import { SelectDeviceButton } from '../../Experiment/SelectDeviceButton';
 import { DeviceOnTrialObj } from '../../objects';
-import { IDeviceOnTrial } from '../../types/types';
 import { ContainedDevicesList } from './ContainedDevicesList';
 import { DeviceLocationEdit } from './DeviceLocationEdit';
 
@@ -21,27 +19,12 @@ export const SingleDevicePropertiesView = ({
   children?: any,
 }) => {
   const { deviceTypeName, deviceItemName } = deviceOnTrial;
-  const { shownMap, changeTrialObj, experiment } = useChosenTrial();
-  const { setExperiment } = useExperiments();
+  const { shownMap, changeTrialObj } = useChosenTrial();
 
   const deviceItem = deviceOnTrial.deviceItem;
   const deviceType = deviceItem.deviceType;
 
   const containedDevices = deviceOnTrial.getContainedDevices();
-
-  const setDeviceOnTrial = (newDeviceData: IDeviceOnTrial | undefined) => {
-    changeTrialObj(draft => {
-      const idx = (draft.devicesOnTrial || []).findIndex(x => x.isSame(deviceOnTrial));
-      if (idx !== -1) {
-        if (!newDeviceData) {
-          draft.devicesOnTrial.splice(idx, 1);
-        } else {
-          const oldDev = draft.devicesOnTrial[idx];
-          draft.devicesOnTrial[idx] = new DeviceOnTrialObj(newDeviceData, oldDev.deviceItem, oldDev.trial);
-        }
-      }
-    });
-  }
 
   return (
     <>
