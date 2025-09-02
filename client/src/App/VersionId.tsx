62689c195cc4e3975b1d77933ba476699926abd6
Eran Geva <eran@kaplanopensource.co.il>
2025-09-02 18:56:38 +0200

    remove try

--- client/src/App/VersionId.tsx
+++ client/src/App/VersionId.tsx
@@ -2,14 +2,14 @@ import { Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 
 export const VersionId = ({ }) => {
-  const [commitId, setCommitId] = useState();
+  const [commitId, setCommitId] = useState('');
   useEffect(() => {
     (async () => {
       try {
         const resp = await fetch("/commit.txt");
         if (resp.ok) {
           const com = await resp.text();
-          setCommitId(com.slice(0, 7) + '_try');
+          setCommitId(com.slice(0, 7));
         } else {
           setCommitId('')
         }
